ad1fa611c2858c4cb297d2de34d2c8f598a1710e
Harika-56 <harika.p@thundersoft.com>
2022-02-13 19:25:31 +0530

    datatype

---
+++ datatypes.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int v1;
+	unsigned int v2;
+	signed int v3;
+	char v4;
+	unsigned char v5;
+	signed char v6;
+	short int v7;
+	unsigned short int v8;
+	signed short int v9;
+	long int v10;
+	unsigned long int v11;
+	signed long int v12;
+	float v13;
+	double v14;
+	long double v15;
+	long long int v16;
+	printf("%lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu",sizeof(v1),sizeof(v2),sizeof(v3),sizeof(v4),sizeof(v5),sizeof(v6),sizeof(v7),sizeof(v8),sizeof(v9),sizeof(v10), sizeof(v11),sizeof(v12),sizeof(v13),sizeof(v14),sizeof(v15),sizeof(v16));
+}
+
