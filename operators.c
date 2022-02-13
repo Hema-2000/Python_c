d671c607ee752e3d8c3acf9aef440c30e6512a3e
Harika-56 <harika.p@thundersoft.com>
2022-02-13 20:26:58 +0530

    operators

---
+++ operators.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,result;
+	printf("enter a and b values");
+	scanf("%d %d",&a,&b);
+	result=a+b;
+	result=a-b;//arithmetic operators
+	result=a/b;
+	result=a%b;
+	result=a&b;
+	result=a|b;//bitwise operators
+	result=a^b;
+	result=a<<b;
+	result=a>>b;
+	if(a>b)//relative operators
+	printf()
+	else
+	printf()	
+	if(a<b)
+	if(a<=b)
+	if(a>=b)
+	if(a==b)
+	if(a>b&&a<b)//logical operator
+	printf()
+	else
+	printf()
+	if(a<b||a>b)
+	if(!a)	
+		
