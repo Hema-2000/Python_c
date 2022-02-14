257d98d43e1e0c48ba0e3b331dc307d8ede01c99
Harika-56 <harika.p@thundersoft.com>
2022-02-14 20:32:19 +0530

    toggle

---
+++ toggle.c
@@ -0,0 +1,10 @@
+#include<,stdio.h>
+int main()
+{
+	int data,bit;
+	printf("enter a data:");
+	scanf("%d",&data);
+	printf("enter bit position:");
+	scanf("%d",&bit);
+	printf("%d",(data^(1<<bit));
+			}			
